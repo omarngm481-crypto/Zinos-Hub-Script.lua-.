336b5e649985d45ebb6a019cd09084223ae9a4dc
Zoins1 <omarngm481@gmail.com>
2026-02-06 14:42:37 +0300

    Update Zoins_Core.lua

--- Zoins_Core.lua
+++ Zoins_Core.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Zoins1/Zinos-Hub-Script.lua/main/Zoins_Main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zoins1/Zinos-Hub-Script/refs/heads/main/Zoins_Core.lua"))()
